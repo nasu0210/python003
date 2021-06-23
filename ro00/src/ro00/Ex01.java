677b40b39d3ff5c227def01c147e1d830e74d76b
HP <HP@DIP-PC>
2021-06-23 12:30:43 +0900

    jjj

---
+++ ro00/src/ro00/Ex01.java
@@ -0,0 +1,17 @@
+package ro00;
+/*
+ * 오늘 날짜:
+ * 개발자:
+ * 개발언어
+ * 
+ */
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+
+	}
+
+}
---
+++ ro00/src/ro00/Test.java
@@ -0,0 +1,20 @@
+package ro00;
+
+import java.util.Calendar;
+
+class Aaa {
+	public String toString(){
+		
+		return "????";
+	}
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Calendar ca=Calendar.getInstance();
+		System.out.println(ca);
+		
+	}
+
+}
